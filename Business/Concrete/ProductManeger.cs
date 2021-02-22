fa051f4eb9fcc5e6c5ed29d39339b5c41d4916af
SAVAŞ METİNOĞLU <76447218+Savasmetin@users.noreply.github.com>
2021-02-22 22:17:49 +0300

    Add project files.

---
+++ Business/Concrete/ProductManeger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManeger : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManeger(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.Unitprice >= min && p.Unitprice <= max);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManeger productManeger = new ProductManeger(new EFProductDal());
+
+            foreach (var product in productManeger.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+
+            }
+         
+        }
+    }
+}
